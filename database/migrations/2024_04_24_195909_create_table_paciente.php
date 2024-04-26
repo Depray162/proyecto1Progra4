89200d5b8400e0beda21118b9d9fc9adf35cd91b
Depray162 <fabricio1quesada@gmail.com>
2024-04-25 18:35:29 -0600

    tercer UP

--- database/migrations/2024_04_24_195909_create_table_paciente.php
+++ database/migrations/2024_04_24_195909_create_table_paciente.php
@@ -1,8 +1,9 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
 
 return new class extends Migration
 {
@@ -11,19 +12,27 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::connection("mysql")-> create('paciente', function (Blueprint $table) {
-            $table->integer("idPaciente")->autoincrement()->primary();
-            $table->string("cedula",50)->nullable();
-            $table->string("nombre",60)->nullable();
-            $table->Integer("edad")->nullable();
-            $table->string("direccion",100);
-            $table->string("telefono",50);
-            $table->string("email",120);
-            
-
-     
-            
+        
+        Schema::connection("mysql")->create('paciente', function (Blueprint $table) {
+            $table->integer("idPaciente")->autoIncrement()->primary();
+            $table->string("cedula", 50)->nullable();
+            $table->string("nombre", 60)->nullable();
+            $table->integer("edad")->nullable();
+            $table->string("direccion", 100);
+            $table->string("telefono", 50);
+            $table->string("email", 120);
         });
+
+        
+        DB::table('paciente')->insert([
+            'cedula' => '1234567890',
+            'nombre' => 'Juan Perez',
+            'edad' => 30,
+            'direccion' => 'Calle 123, Ciudad ABC',
+            'telefono' => '123456789',
+            'email' => 'juan@example.com',
+        ]);
+
     }
 
     /**
