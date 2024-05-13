54ec80e564449e97deeae7312a3a518b239b8b14
Dydagope <dydagope@gmail.com>
2024-05-12 23:50:54 -0600

    EntidadCita1

--- routes/api.php
+++ routes/api.php
@@ -1,41 +1,26 @@
 <?php
 
-
-use App\Http\Controllers\HistorialController;
-
-use App\Http\Controllers\ExpedienteController;
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PacienteController;
-
-use App\Models\Expediente;
-use App\Models\paciente;
 use App\Http\Controllers\MedicoController;
-use App\Models\Medico;
-
-
+use App\Http\Controllers\CitaController;
+use App\Http\Controllers\HistorialController;
+use App\Http\Controllers\ExpedienteController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::prefix('v1')->group( 
-    function(){
-//Rutas expecificas
+Route::prefix('v1')->group(
+    function () {
+        //Rutas expecificas
 
-//rutas automaticas
-
-  Route::resource('/paciente',PacienteController::class, ['except'=> ['create', 'edit']]);
-Route::resource('/expediente',ExpedienteController::class, ['except'=> ['create', 'edit']]);
-
-
-Route::resource('/paciente',PacienteController::class);
-Route::resource('/medico',MedicoController::class,['except'=> ['create', 'edit']]);
-Route::resource('/paciente',PacienteController::class, ['except'=> ['create', 'edit']]);
-Route::resource('/historial',HistorialController::class,['except'=> ['create', 'edit']]);
-    } 
-
-
+        //rutas automaticas
+        Route::resource('/paciente', PacienteController::class, ['except' => ['create', 'edit']]);
+        Route::resource('/medico', MedicoController::class, ['Except' => ['create', 'edit']]);
+        Route::resource('/cita', CitaController::class, ['Except' => ['create', 'edit']]);
+        Route::resource('/historial', HistorialController::class, ['Except' => ['create', 'edit']]);
+        route::resource('/expediente', ExpedienteController::class, ['Except' => ['create', 'edit']]);
+    }
 );
-
